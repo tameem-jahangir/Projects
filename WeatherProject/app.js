a960a1657e0e6def5e08f0d2c0502c66192ac820
tameem.j <44329967+tameem-jahangir@users.noreply.github.com>
2022-10-11 00:11:28 +0200

    Update app.js

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -7,7 +7,12 @@ app.get("/", function(req, res){
     const url = "https://catfact.ninja/fact";
 
     https.get(url, function(response){
-        console.log(response);
+        console.log(response.statusCode);
+
+        response.on("data", function(data){
+            const catdata = JSON.parse(data);
+            console.log(catdata);
+        });
     })
 
     res.send("Server is running.");
